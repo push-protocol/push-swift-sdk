c76a10c679a858233324b3ae11d8a35f93263dd4
Abishek Bashyal <abishek_bashyal@outlook.com>
2023-06-09 14:31:24 +0545

    refactor:decrypt message

--- Sources/Chat/Conversation.swift
+++ Sources/Chat/Conversation.swift
@@ -25,24 +25,29 @@ extension PushChat {
     }
   }
 
-  public static func Latest(threadHash: String, pgpPrivateKey: String, env: ENV) async throws
+  public static func Latest(threadHash: String, pgpPrivateKey: String, toDecrypt: Bool, env: ENV)
+    async throws
     -> Message
   {
     return try await History(
-      threadHash: threadHash, limit: 1, pgpPrivateKey: pgpPrivateKey, env: env
+      threadHash: threadHash, limit: 1, pgpPrivateKey: pgpPrivateKey, toDecrypt: toDecrypt, env: env
     ).first!
   }
 
-  public static func History(threadHash: String, limit: Int, pgpPrivateKey: String, env: ENV)
+  public static func History(
+    threadHash: String, limit: Int, pgpPrivateKey: String, toDecrypt: Bool, env: ENV
+  )
     async throws -> [Message]
   {
     do {
       var messages = try await getMessagesService(threadHash: threadHash, limit: limit, env: env)
 
-      for i in 0..<messages.count {
-        let decryptedMsg = try decryptMessage(
-          message: messages[i], privateKeyArmored: pgpPrivateKey)
-        messages[i].messageContent = decryptedMsg
+      if toDecrypt {
+        for i in 0..<messages.count {
+          let decryptedMsg = try decryptMessage(
+            message: messages[i], privateKeyArmored: pgpPrivateKey)
+          messages[i].messageContent = decryptedMsg
+        }
       }
 
       return messages
@@ -78,11 +83,25 @@ extension PushChat {
       if message.encType != "pgp" {
         return message.messageContent
       }
+      return try decryptMessage(
+        message.messageContent, encryptedSecret: message.encryptedSecret,
+        privateKeyArmored: privateKeyArmored)
+    } catch {
+      return "Unable to decrypt message"
+    }
+  }
+
+  public static func decryptMessage(
+    _ message: String,
+    encryptedSecret: String,
+    privateKeyArmored: String
+  ) throws -> String {
+    do {
 
       let secretKey = try Pgp.pgpDecrypt(
-        cipherText: message.encryptedSecret, toPrivateKeyArmored: privateKeyArmored)
+        cipherText: encryptedSecret, toPrivateKeyArmored: privateKeyArmored)
 
-      let userMsg = AESCBCHelper.decrypt(cipherText: message.messageContent, secretKey: secretKey)!
+      let userMsg = AESCBCHelper.decrypt(cipherText: message, secretKey: secretKey)!
       let userMsgStr = String(data: userMsg, encoding: .utf8)
 
       if userMsgStr == nil {
@@ -94,5 +113,4 @@ extension PushChat {
       return "Unable to decrypt message"
     }
   }
-
 }
